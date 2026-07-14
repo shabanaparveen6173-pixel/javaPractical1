1cc308d7aa39ed430debf3da6d1bc3cfadb7c48e
shabanaparveen6173-pixel <shabanaparveen6173@gmail.com>
2026-07-14 10:54:56 +0530

    Add files via upload

---
+++ Practical2.java
@@ -0,0 +1,16 @@
+public class Practical2 {
+    public static void main(String[] args) {
+
+        int num1 = 20;
+        int num2 = 25;
+
+        int sum = num1 + num2;
+
+        System.out.println("Number 1:" + num1);
+        System.out.println("Number 2:" + num2);
+        System.out.println("Addition of:" + num1);
+        System.out.println("and:" + num2);
+        System.out.println("is:" + sum);
+        System.out.println();
+    }
+}
