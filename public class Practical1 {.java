14698c57c2a18a94940e05da90b1a5a6736e4881
shabanaparveen6173-pixel <shabanaparveen6173@gmail.com>
2026-07-14 10:29:56 +0530

    Add files via upload

---
+++ public class Practical1 {.java
@@ -0,0 +1,41 @@
+
+public class main {
+        public static void main(String[] args) {
+
+            // Hello World
+            System.out.println("HEllo World!");
+            System.out.println();
+
+            // Variables and Data types
+            int age = 20;
+            double salary = 35000.75;
+            char grade = 'A';
+            boolean isStudent = true;
+            String name = "Rahul";
+
+            System.out.println("Name:" + name);
+            System.out.println("Age:" + age);
+            System.out.println("Salary:" + salary);
+            System.out.println("Grade:" + grade);
+            System.out.println("Student:" + isStudent);
+            System.out.println();
+
+            // Implicit Type Casting (Automatic)
+            int number = 50;
+            double result = number;
+
+            System.out.println("Implicit Type Casting");
+            System.out.println("Integer Value:" + number);
+            System.out.println("Converted To Double: " + result);
+
+            // Explicit Type Casting(Manual)
+            double marks = 89.75;
+            int totalMarks = (int) marks;
+
+            System.out.println("Explicit Type Casting");
+            System.out.println("Double Value:" + marks);
+            System.out.println("Converted To Integer: " + totalMarks);
+        }
+    }
+
+}
